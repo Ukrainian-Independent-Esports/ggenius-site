7c24729c05471927a3453ded720adbdd786bd017
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-02-08 21:54:58 +0200

    profile style

--- src/page/Profile/Profile.jsx
+++ src/page/Profile/Profile.jsx
@@ -4,14 +4,19 @@ import useLangChange from '../../Hooks/LangChange';
 import { useAuth } from '../../Hooks/useAuth';
 
 
+import userBack from './img/userBackground.png'
+import iconId from './img/icon-id.svg'
+import iconRole from './img/icon-role.svg'
+import iconLocation from './img/icon-location.svg'
+
 const Profile = () => {
   const auth = useAuth();
   const { user } = auth || {};
-  const [zoom, setZoom] = useState(false)
-  const [zoom1, setZoom1] = useState(false)
-
+  const [nav, setNav] = useState(1)
   const t = useLangChange()
   console.log(user);
+  console.log(nav);
+
 
   const raw = user.main_roles
   const arr = JSON.parse(raw)
@@ -23,85 +28,45 @@ const Profile = () => {
       <div className={style.container}>
         <div className={style.profileInner}>
           <div className={style.profileInnerUserInfo}>
-            <div className={style.profileInnerUserInfoAvatarEff}>
-              <img src={user.avatar_permanent_url !== null ? user.avatar_permanent_url : ''} alt={user.avatar_permanent_url !== null ? 'avatar' : ''} className={style.profileInnerUserInfoImg} />
-              <span className={style.profileInnerUserInfoAvatarEff1}></span>
-            </div>
-            <div className={style.profileInnerUserInfoTxt}>
-              <h1 className={style.profileInnerUserInfoTitle}>
-                {user.nickname}
-              </h1>
-              <p className={style.profileInnerUserInfoRank}>
-                {user.current_rank}
-              </p>
-
-              <p className={style.profileInnerUserInfoRank}>
-                Основна роль: {arr !== null ? arr.map((item, ind) => (
-                  <span key={ind}>
-                    {item}{ind < arr.length - 1 ? ', ' : ''}
-                  </span>
-                )) : 'Завантажте скрин з гри у бота'}
-              </p>
-              <p className={style.profileInnerUserInfoRank}>
-                Загальна кількість матчів: {user.total_matches !== null ? user.total_matches : 'Завантажте скрин з гри у бота'}
-              </p>
-              <p className={style.profileInnerUserInfoRank}>
-                Загальни він рейт: {user.win_rate !== null ? user.win_rate : 'Завантажте скрин з гри у бота'}%
-
-              </p>
+            <div className={style.profileInnerUserInfoObv}>
+              <img className={style.profileInnerUserInfoBack} src={userBack} alt="" />
+              <div className={style.profileInnerUserInfoAvatarEff}>
+                <img src={user.avatar_permanent_url !== null ? user.avatar_permanent_url : ''} alt={user.avatar_permanent_url !== null ? 'avatar' : ''} className={style.profileInnerUserInfoImg} />
+                <span className={style.profileInnerUserInfoAvatarEff1}></span>
+              </div>
+              <div className={style.profileInnerUserInfoTxt}>
+                <h1 className={style.profileInnerUserInfoTitle}>
+                  {user.nickname}
+                </h1>
+                <p className={style.profileInnerUserInfoRank}>
+                  <img src={iconId} alt="" /> {t('profileInnerUserInfoRankId')} {user.player_id}({user.server_id})
+                </p>
+                <p className={style.profileInnerUserInfoRank}>
+                  <img src={iconRole} alt="" /> {t('profileInnerUserInfoRankRole')} {arr !== null ? arr.map((item, ind) => (
+                    <span key={ind}>
+                      {item}{ind < arr.length - 1 ? ', ' : ''}
+                    </span>
+                  )) : 'Завантажте скрин з гри у бота'}
+                </p>
+                <p className={style.profileInnerUserInfoRank}>
+                  <img src={iconLocation} alt="icon location" /> {t('profileInnerUserInfoRankLocation')} {user.location !== null ? user.location : 'Завантажте скрин з гри у бота'}
+                </p>
+              </div>
             </div>
           </div>
-          <div className={style.profileInnerUserChara}>
-            <h2 className={style.profileInnerUserCharaTitle}>
-              Ігровий профіль
-            </h2>
-            <div className={style.profileInnerUserCharaImages}>
-              <img src={user.basic_profile_permanent_url}
-                alt={user.basic_profile_permanent_url !== null ? "photo game profile" : 'Завантажте скрин профілю з гри у бота'}
-                className={!zoom ? style.profileInnerUserCharaImg : style.profileInnerUserCharaImgZoom}
-                onClick={() => setZoom(!zoom)} />
-              <img src={user.heroes_photo_permanent_url}
-                alt={user.heroes_photo_permanent_url !== null ? "photo game hero" : 'Завантажте скрин улюблених героїв, з гри у бота'}
-                className={!zoom1 ? style.profileInnerUserCharaImg1 : style.profileInnerUserCharaImgZoom1}
-                onClick={() => setZoom1(!zoom1)} />
+        </div>
+        
+        <div className={style.profileInnerHeader}>
+          <ul className={style.profileInnerHeaderList}>
+            <li className={nav == 1 ? style.profileInnerHeaderListItem : style.profileInnerHeaderListItemOff} onClick={() => setNav(1)}>{t('profileInnerHeaderListItemTotal')}</li>
+            <li className={nav == 2 ? style.profileInnerHeaderListItem : style.profileInnerHeaderListItemOff} onClick={() => setNav(2)}>{t('profileInnerHeaderListItemDetail')}</li>
+            <li className={nav == 3 ? style.profileInnerHeaderListItem : style.profileInnerHeaderListItemOff} onClick={() => setNav(3)}>{t('profileInnerHeaderListItemSettings')}</li>
+          </ul>
+                  
+          <div className={style.profileInnerHeaderInfo}>
+            <div className={style.profileInnerHeaderInfoNav}>
+                  {nav}
             </div>
-            <h2 className={style.profileInnerUserCharaTitleHero}>Улюблені герої</h2>
-            <ul className={style.profileInnerUserCharaList}>
-              <li className={style.profileInnerUserCharaListItem}>
-                <h2 className={style.profileInnerUserCharaListItemTitle}>
-                  {user.hero1_name !== null ? user.hero1_name : 'Немає даних'}
-                </h2>
-                <p className={style.profileInnerUserCharaListItemSub}>
-                  Зіграно матчів на герої: {user.hero1_matches !== null ? user.hero1_matches : 'Немає даних'}
-                </p>
-                <p className={style.profileInnerUserCharaListItemSub}>
-                  Win rate: {user.hero1_win_rate !== null ? user.hero1_win_rate : 'Немає даних'}%
-                </p>
-              </li>
-              <li className={style.profileInnerUserCharaListItem}>
-                <h2 className={style.profileInnerUserCharaListItemTitle}>
-                  {user.hero2_name !== null ? user.hero2_name : 'Немає даних'}
-                </h2>
-                <p className={style.profileInnerUserCharaListItemSub}>
-                  Зіграно матчів на герої: {user.hero2_matches !== null ? user.hero2_matches : 'Немає даних'}
-                </p>
-                <p className={style.profileInnerUserCharaListItemSub}>
-                  Win rate: {user.hero2_win_rate !== null ? user.hero2_win_rate : 'Немає даних'}%
-                </p>
-              </li>
-              <li className={style.profileInnerUserCharaListItem}>
-                <h2 className={style.profileInnerUserCharaListItemTitle}>
-                  {user.hero3_name !== null ? user.hero3_name : 'Немає даних'}
-                </h2>
-                <p className={style.profileInnerUserCharaListItemSub}>
-                  Зіграно матчів на герої: {user.hero3_matches !== null ? user.hero3_matches  : 'Немає даних'}
-                </p>
-                <p className={style.profileInnerUserCharaListItemSub}>
-                  Win rate: {user.hero3_win_rate !== null ? user.hero3_win_rate : 'Немає даних'}%
-                </p>
-              </li>
-            </ul>
-
           </div>
         </div>
       </div>
