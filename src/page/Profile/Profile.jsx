ef388ebe5ceb6bbbdd473d091c029ea08784b196
Nimarchik <141066912+Nimarchik@users.noreply.github.com>
2026-02-10 11:25:53 +0200

    safe profile gg

--- src/page/Profile/Profile.jsx
+++ src/page/Profile/Profile.jsx
@@ -17,8 +17,16 @@ const Profile = () => {
   const [btn, setBtn] = useState(3)
   const t = useLangChange()
   console.log(user);
-  console.log(nav);
 
+  const procent = (value) => {
+    const percent = (value / 10000) * 100
+    return percent.toFixed(2)
+  }
+
+  const procentMax = (value) => {
+    const percent = (value / 20000) * 100
+    return percent.toFixed(2)
+  }
 
   const raw = user.main_roles
   const arr = JSON.parse(raw)
@@ -121,18 +129,19 @@ const Profile = () => {
                             <li className={style.profileInnerHeaderInfo1TotalStatListItem}>
                               <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImg} />
                               <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
-                                {t('profileInnerHeaderInfo1TotalStatListItemTitleMaksRank')}
+                                {t('profileInnerHeaderInfo1TotalStatListItemTitleMaksMinRank')}
                               </h4>
                               <p className={style.profileInnerHeaderInfo1TotalStatListItemSub}>
-                                {user.highest_rank}
+                                {user.current_rank}
                               </p>
                             </li>
                             <li className={style.profileInnerHeaderInfo1TotalStatListItem}>
                               <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImg} />
                               <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
-                                {t('profileInnerHeaderInfo1TotalStatListItemTitle')}
+                                {t('profileInnerHeaderInfo1TotalStatListItemTitleMaksMaksRank')}
                               </h4>
                               <p className={style.profileInnerHeaderInfo1TotalStatListItemSub}>
+                                {user.highest_rank}
 
                               </p>
                             </li>
@@ -144,32 +153,108 @@ const Profile = () => {
                           <h4 className={style.profileInnerHeaderInfo1TotalStatGroup1DetailTitle}>
                             {t('profileInnerHeaderInfo1TotalStatGroup1DetailTitle')}
                           </h4>
-                          <ul className={style.profileInnerHeaderInfo1TotalStatList}>
-                            <li className={style.profileInnerHeaderInfo1TotalStatListItem}>
-                              <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImg} />
-                              <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
-                                {t('profileInnerHeaderInfo1TotalStatListItemTitle')}
-                              </h4>
-                              <p className={style.profileInnerHeaderInfo1TotalStatListItemSub}>
-
+                          <ul className={style.profileInnerHeaderInfo1TotalStatListDet}>
+                            <li className={style.profileInnerHeaderInfo1TotalStatListItemDet}>
+                              <div className={style.profileInnerHeaderInfo1TotalStatListItemHed}>
+                                <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImgDet} />
+                                <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
+                                  {t('profileInnerHeaderInfo1TotalStatListItemTitleKDA')}
+                                </h4>
+                              </div>
+                              <p className={style.profileInnerHeaderInfo1TotalStatListItemSubDet}>
+                                {user.kda_ratio}
                               </p>
                             </li>
-                            <li className={style.profileInnerHeaderInfo1TotalStatListItem}>
-                              <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImg} />
-                              <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
-                                {t('profileInnerHeaderInfo1TotalStatListItemTitle')}
-                              </h4>
-                              <p className={style.profileInnerHeaderInfo1TotalStatListItemSub}>
-
+                            <li className={style.profileInnerHeaderInfo1TotalStatListItemDet}>
+                              <div className={style.profileInnerHeaderInfo1TotalStatListItemHed}>
+                                <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImgDet} />
+                                <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
+                                  {t('profileInnerHeaderInfo1TotalStatListItemTitleDead')}
+                                </h4>
+                              </div>
+                              <p className={style.profileInnerHeaderInfo1TotalStatListItemSubDet}>
+                                {user.avg_deaths_per_match}
                               </p>
                             </li>
-                            <li className={style.profileInnerHeaderInfo1TotalStatListItem}>
-                              <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImg} />
-                              <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
-                                {t('profileInnerHeaderInfo1TotalStatListItemTitle')}
-                              </h4>
-                              <p className={style.profileInnerHeaderInfo1TotalStatListItemSub}>
+                            <li className={style.profileInnerHeaderInfo1TotalStatListItemDet}>
+                              <div className={style.profileInnerHeaderInfo1TotalStatListItemHed}>
+                                <img src={iconInfo} alt="" className={style.profileInnerHeaderInfo1TotalStatListItemImgDet} />
+                                <h4 className={style.profileInnerHeaderInfo1TotalStatListItemTitle}>
+                                  {t('profileInnerHeaderInfo1TotalStatListItemTitleHelp')}
+                                </h4>
+                              </div>
+                              <p className={style.profileInnerHeaderInfo1TotalStatListItemSubDet}>
+                                {user.most_assists_in_one_game}
+                              </p>
+                            </li>
+                          </ul>
+                        </div>
 
+                        {/* Економіка та шкода ( урон)  */}
+                        <div className={style.profileInnerEconomy}>
+                          <h5 className={style.profileInnerEconomyTitle}>
+                            {t('profileInnerEconomyTitle')}
+                          </h5>
+
+                          <ul className={style.profileInnerEconomyList}>
+                            {/* GOLD */}
+                            <li className={style.profileInnerEconomyListItem}>
+                              <div className={style.profileInnerEconomyListItemHeads}>
+                                <img src={iconInfo} alt="icon" className={style.profileInnerEconomyListItemHeadsImg} />
+                                <h5 className={style.profileInnerEconomyListItemHeadsTitle}>
+                                  {t('profileInnerEconomyListItemHeadsTitleGold')}
+                                </h5>
+                              </div>
+                              <div className={style.profileInnerEconomyListItemBack}>
+                                <div className={style.profileInnerEconomyListItemBackStroke} style={{ '--value': procent(user.avg_gold_per_min) }}>
+                                  <span className={style.profileInnerEconomyListItemBackStrokeInfo}>
+                                    {procent(user.avg_gold_per_min)}%
+                                  </span>
+                                </div>
+                              </div>
+                              <p className={style.profileInnerEconomyListItemSubGold}>
+                                {user.avg_gold_per_min}
+                              </p>
+                            </li>
+                            {/* DAMEG */}
+                            <li className={style.profileInnerEconomyListItem}>
+                              <div className={style.profileInnerEconomyListItemHeads}>
+                                <img src={iconInfo} alt="icon" className={style.profileInnerEconomyListItemHeadsImg} />
+                                <h5 className={style.profileInnerEconomyListItemHeadsTitle}>
+                                  {t('profileInnerEconomyListItemHeadsTitleDmg')}
+                                </h5>
+                              </div>
+                              <div className={style.profileInnerEconomyListItemBack}>
+                                <div className={style.profileInnerEconomyListItemBackStroke} style={{ '--value': procent(user.avg_hero_dmg_per_min) }}>
+                                  <span className={style.profileInnerEconomyListItemBackStrokeInfo}>
+                                    {procent(user.avg_hero_dmg_per_min)}%
+                                  </span>
+
+                                </div>
+                              </div>
+                              <p className={style.profileInnerEconomyListItemSubDMG}>
+                                {user.avg_hero_dmg_per_min}
+                              </p>
+                            </li>
+                            {/* turret DMG */}
+                            <li className={style.profileInnerEconomyListItem}>
+                              <div className={style.profileInnerEconomyListItemHeads}>
+                                <img src={iconInfo} alt="icon" className={style.profileInnerEconomyListItemHeadsImg} />
+                                <h5 className={style.profileInnerEconomyListItemHeadsTitle}>
+                                  {t('profileInnerEconomyListItemHeadsTitleCurDmg')}
+                                </h5>
+                              </div>
+                              <div className={style.profileInnerEconomyListItemBack} >
+                                <div className={style.profileInnerEconomyListItemBackStroke} style={{ '--value': procent(user.avg_turret_dmg_per_match) }}>
+                                  <span className={style.profileInnerEconomyListItemBackStrokeInfo}>
+                                    {
+                                      procent(user.avg_turret_dmg_per_match)
+                                    }%
+                                  </span>
+                                </div>
+                              </div>
+                              <p className={style.profileInnerEconomyListItemSubTurDMG}>
+                                {user.avg_turret_dmg_per_match}
                               </p>
                             </li>
                           </ul>
@@ -177,8 +262,72 @@ const Profile = () => {
                       </div>
 
                       {/* СЕРЕДНЯ ЗА МАТЧ СТАТИСТИКА */}
-                      <div className={style.profileInnerHeaderInfo1TotalMatches}>
-                        gg
+                      <div className={style.profileInnerTotalMatches}>
+                        {/* СРЕДНЕЕ */}
+                        <div className={style.profileInnerTotalMatchesАverage}>
+                          <h5 className={style.profileInnerTotalMatchesАverageTitle}>
+                            {t('profileInnerTotalMatchesАverageTitle')}
+                          </h5>
+
+                          <ul className={style.profileInnerTotalMatchesАverageList}>
+                            <li className={style.profileInnerTotalMatchesАverageListItem}>
+                              <div className={style.profileInnerTotalMatchesАverageListItemHed}>
+                                <span className={style.profileInnerTotalMatchesАverageListItemHed}>
+                                  <img src={iconInfo} className={style.profileInnerTotalMatchesАverageListItemHedImg} alt="" />
+                                  <h5 className={style.profileInnerTotalMatchesАverageListItemHeadTitle}>
+                                    {t('profileInnerTotalMatchesАverageListItemHeadTitleGold')}
+                                  </h5>
+                                </span>
+                                <p className={style.profileInnerTotalMatchesАverageListItemHedSub}>
+                                  {user.highest_gold_per_min}
+                                </p>
+                              </div>
+
+                              <div className={style.profileInnerTotalMatchesАverageListItemBack} style={{ '--valueMax': procentMax(user.highest_gold_per_min) }}>
+                                <span></span>
+                              </div>
+                            </li>
+                            <li className={style.profileInnerTotalMatchesАverageListItem}>
+                              <div className={style.profileInnerTotalMatchesАverageListItemHed}>
+                                <span className={style.profileInnerTotalMatchesАverageListItemHed}>
+                                  <img src={iconInfo} className={style.profileInnerTotalMatchesАverageListItemHedImg} alt="" />
+                                  <h5 className={style.profileInnerTotalMatchesАverageListItemHeadTitle}>
+                                    {t('profileInnerTotalMatchesАverageListItemHeadTitleDMG')}
+                                  </h5>
+                                </span>
+                                <p className={style.profileInnerTotalMatchesАverageListItemHedSub}>
+                                  {user.highest_dmg_per_min}
+                                </p>
+                              </div>
+                              <div className={style.profileInnerTotalMatchesАverageListItemBack} style={{ '--valueMax': procentMax(user.highest_dmg_per_min) }}>
+                                <span></span>
+                              </div>
+                            </li>
+                            <li className={style.profileInnerTotalMatchesАverageListItem}>
+                              <div className={style.profileInnerTotalMatchesАverageListItemHed}>
+                                <span className={style.profileInnerTotalMatchesАverageListItemHed}>
+
+                                  <img src={iconInfo} className={style.profileInnerTotalMatchesАverageListItemHedImg} alt="" />
+                                  <h5 className={style.profileInnerTotalMatchesАverageListItemHeadTitle}>
+                                    {t('profileInnerTotalMatchesАverageListItemHeadTitleTurDMG')}
+                                  </h5>
+                                </span>
+                                <p className={style.profileInnerTotalMatchesАverageListItemHedSub}>
+                                  {user.highest_dmg_taken_per_min}
+                                </p>
+                              </div>
+                              <div className={style.profileInnerTotalMatchesАverageListItemBack} style={{ '--valueMax': procentMax(user.highest_dmg_taken_per_min) }}>
+                                <span></span>
+                              </div>
+                            </li>
+                          </ul>
+                        </div>
+
+                        <div className={style.profileInnerTotalMatchesAchievement}>
+                          <h5 className={style.profileInnerTotalMatchesAchievementTitle}>
+                            {t('profileInnerTotalMatchesAchievementTitle')}
+                          </h5>
+                        </div>
                       </div>
                     </div>
                   </div>
